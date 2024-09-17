cc960fefb5ac61a69420d5f90b87c2158ca76419
O Moura <123185795+mouraluck@users.noreply.github.com>
2024-09-17 12:42:35 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+// window.addEventListener("scroll", function () {
+//   const scrollPosition = window.scrollY;
+//   const item = document.querySelector(".animated-item");
+//   const moveUp = Math.min(scrollPosition * 0.5, 100); // Limita a animação a 100px
+//   item.style.transform = `translateY(${100 - moveUp}px)`;
+// });
